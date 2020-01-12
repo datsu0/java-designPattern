8a340706d9a4746ebb25cf42b20b62e6eaa9d534
datsu0 <atsu34412@gmail.com>
2020-01-12 20:33:24 +0900

    facade

--- AbstractFactory/listfactory/ListFactory.java
+++ AbstractFactory/listfactory/ListFactory.java
@@ -8,7 +8,7 @@ public class ListFactory extends Factory{
     public Tray createTray(String caption){
         return new ListTray(caption);
     }
-    public Page cretePage(String title, String author){
+    public Page createPage(String title, String author){
         return new ListPage(title, author);
     }
 }
---
+++ Composite/Main.java
@@ -0,0 +1,36 @@
+public class Main{
+    public static void main(String[] args){
+        try {
+            System.out.println("making root entrise...");
+            Directory rootdir = new Directory("root");
+            Directory bindir = new Directory("bin");
+            Directory tmpdir = new Directory("tmp");
+            Directory usrdir = new Directory("usr");
+            rootdir.add(bindir);
+            rootdir.add(tmpdir);
+            rootdir.add(usrdir);
+            bindir.add(new File("vi",1000));
+            bindir.add(new File("latex",20000));
+            rootdir.printList();
+
+            System.out.println("");
+            System.out.println("Making user entries...");
+            Directory yuki = new Directory("yuki");
+            Directory hanako = new Directory("hanako");
+            Directory tomura = new Directory("tomura");
+            usrdir.add(yuki);
+            usrdir.add(hanako);
+            usrdir.add(tomura);
+            yuki.add(new File("diary.html", 100));
+            yuki.add(new File("Composite.java", 200));
+            hanako.add(new File("memo.tex",300));
+            tomura.add(new File("memo.doc", 400));
+            tomura.add(new File("junk.mail", 500));
+            rootdir.printList();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            //TODO: handle exception
+        }
+    }
+}
